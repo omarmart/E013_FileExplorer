1e9f65f900b810e22b8c08889f77d4b25cca6192
Omar <martinez.trujillo.omar@gmail.com>
2021-04-22 20:17:04 +0200

    add test getFolderTree

--- src/main/java/com/omar/ExplorerWindow.java
+++ src/main/java/com/omar/ExplorerWindow.java
@@ -5,13 +5,23 @@ import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
+import javax.swing.text.html.HTMLDocument.HTMLReader.FormAction;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ExplorerWindow extends JFrame implements ActionListener {
 
     public ExplorerWindow() {
+        /*
+        Each folder/file is its own button? 
+        They can share the click event but 
+        Make own object that saves information of folder/file?
+        */
+
         Box box = new Box(BoxLayout.PAGE_AXIS);
         JLabel usernameField = new JLabel("Patata");
         box.add(usernameField);
@@ -20,11 +30,19 @@ public class ExplorerWindow extends JFrame implements ActionListener {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(800, 800);
         setVisible(true);
+        getFolderFileList();
+    }
+
+    public void getFolderFileList() {
+        //how to ignore system files and folders ????
+        File rootPath = new File("C:\\");
+        for (String file : rootPath.list()) {
+            System.out.println(file);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
 
     }
 
